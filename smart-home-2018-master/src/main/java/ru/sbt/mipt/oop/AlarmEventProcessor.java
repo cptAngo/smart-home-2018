e04c71e9876ea0bade22e4f4937f97b562848407
cptAngo <leha.andreevich@yandex.ru>
2018-12-08 00:05:19 +0300

    Update AlarmEventProcessor.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
@@ -1,21 +1,32 @@
 package ru.sbt.mipt.oop;
 
+import java.lang.reflect.Field;
+
 public class AlarmEventProcessor implements EventProcessor {
     @Override
     public void processEvent(SmartHome smartHome, SensorEvent sensorEvent) {
         if(this.isAlarmEvent(sensorEvent)) {
             AlarmSensorEvent event = (AlarmSensorEvent) sensorEvent;
-            if (sensorEvent.getType() == SensorEventType.ALARM_ACTIVATE) {
-                smartHome.getAlarmSystem().activate(event.getCode());
-            }
-            else {
-                smartHome.getAlarmSystem().deactivate(event.getCode());
-            }
-        }
-        else {
-            if (smartHome.getAlarmSystem().getState().equals(AlarmSystemState.ON)) {
-                smartHome.getAlarmSystem().setAlarm();
-                System.out.println("Sending sms");
+            try {
+                Field field = smartHome.getAlarmSystem().getClass().getDeclaredField("pass");
+                field.setAccessible(true);
+                if (event.getCode().equals(field.get(smartHome.getAlarmSystem()))) {
+                    if (sensorEvent.getType() == SensorEventType.ALARM_ACTIVATE) {
+                        ActivateAlarmState activate = new ActivateAlarmState(smartHome.getAlarmSystem());
+                        activate.activate();
+                    }
+                    else if(sensorEvent.getType() == SensorEventType.ALARM_DEACTIVATE) {
+                        DeactivateAlarmState deactivate = new DeactivateAlarmState(smartHome.getAlarmSystem());
+                        deactivate.deactivate();
+                    }
+                }
+                else {
+                    SetAlarmState setAlarmState = new SetAlarmState(smartHome.getAlarmSystem());
+                    setAlarmState.setAlarm();
+                    System.out.println("Sending sms");
+                    }
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                e.printStackTrace();
             }
         }
     }
