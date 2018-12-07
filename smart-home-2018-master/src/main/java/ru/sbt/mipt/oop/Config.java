a221020dc7cf3e0cee9e69e6f4e97be94215bea4
cptAngo <leha.andreevich@yandex.ru>
2018-12-08 00:09:21 +0300

    Update Config.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/Config.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/Config.java
@@ -1,19 +1,32 @@
 package ru.sbt.mipt.oop;
 
-import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+
 @Configuration
 public class Config {
+    private SmartHome smartHome;
+    private SensorEventProvider sensorEventProvider;
+    private EventsManager eventsManager;
+    private SmartHomeLoader smartHomeLoader = new FileSmartHomeLoader();
+
+    public Config() throws IOException {
+        smartHome = smartHomeLoader.loadSmartHome();
+        sensorEventProvider = new RandomSensorEventProvider();
+        eventsManager = new HomeEventsObserver(sensorEventProvider);
+        //eventsManager = new EventsManagerAdapter(new SensorEventsManager());
+    }
+
 
     @Bean
-    SmartHomeLoader smartHomeLoader() {
-        return new FileSmartHomeLoader();
+    SmartHome getSmartHome() {
+        return smartHome;
     }
 
     @Bean
-    EventsManager eventsManager() {
-        return new EventsManagerAdapter(new SensorEventsManager());
+    EventsManager getEventsManager() {
+        return eventsManager;
     }
 }
