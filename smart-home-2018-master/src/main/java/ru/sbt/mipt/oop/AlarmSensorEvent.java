ea626034ee2b66fb5f02afbe1368f9fcbbc67b4c
cptAngo <leha.andreevich@yandex.ru>
2018-12-14 23:47:03 +0300

    Update AlarmSensorEvent.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmSensorEvent.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmSensorEvent.java
@@ -1,11 +1,15 @@
 package ru.sbt.mipt.oop;
 
 public class AlarmSensorEvent extends SensorEvent{
-    private String code;
+    public String code;
 
     public AlarmSensorEvent(SensorEventType type, String code) {
         super(type, "alarm");
         this.code = code;
+    }
+
+    public SensorEventType getType() {
+        return super.getType();
     }
 
     public String getCode() {
