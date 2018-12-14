7a760917ceca8abc22363ed55ebf981549dfeafb
cptAngo <leha.andreevich@yandex.ru>
2018-12-14 23:49:55 +0300

    Update AlarmEventProcessor.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
@@ -1,32 +1,25 @@
 package ru.sbt.mipt.oop;
 
-import java.lang.reflect.Field;
-
 public class AlarmEventProcessor implements EventProcessor {
     @Override
     public void processEvent(SmartHome smartHome, SensorEvent sensorEvent) {
         if(this.isAlarmEvent(sensorEvent)) {
             AlarmSensorEvent event = (AlarmSensorEvent) sensorEvent;
-            try {
-                Field field = smartHome.getAlarmSystem().getClass().getDeclaredField("pass");
-                field.setAccessible(true);
-                if (event.getCode().equals(field.get(smartHome.getAlarmSystem()))) {
-                    if (sensorEvent.getType() == SensorEventType.ALARM_ACTIVATE) {
-                        ActivateAlarmState activate = new ActivateAlarmState(smartHome.getAlarmSystem());
-                        activate.activate();
-                    }
-                    else if(sensorEvent.getType() == SensorEventType.ALARM_DEACTIVATE) {
-                        DeactivateAlarmState deactivate = new DeactivateAlarmState(smartHome.getAlarmSystem());
-                        deactivate.deactivate();
-                    }
+            if (sensorEvent.getType() == SensorEventType.ALARM_ACTIVATE) {
+                ActivateAlarmState activate = new ActivateAlarmState(smartHome.getAlarmSystem());
+                if (event.getCode().hashCode() == smartHome.getAlarmSystem().getCode()) {
+                    activate.activate();
                 }
-                else {
-                    SetAlarmState setAlarmState = new SetAlarmState(smartHome.getAlarmSystem());
-                    setAlarmState.setAlarm();
-                    System.out.println("Sending sms");
-                    }
-            } catch (IllegalAccessException | NoSuchFieldException e) {
-                e.printStackTrace();
+            }
+            else {
+                DeactivateAlarmState deactivate = new DeactivateAlarmState(smartHome.getAlarmSystem());
+                if (event.getCode().hashCode() == smartHome.getAlarmSystem().getCode()) {
+                    deactivate.deactivate();
+                }
+            }
+            SetAlarmState alarmState = new SetAlarmState(smartHome.getAlarmSystem());
+            if (event.getCode().hashCode() != smartHome.getAlarmSystem().getCode()) {
+                alarmState.setAlarm();
             }
         }
     }
