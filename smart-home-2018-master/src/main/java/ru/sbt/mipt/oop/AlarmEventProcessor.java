5ca413afd7900683cefcba3ce580d863a5cc5e59
cptAngo <leha.andreevich@yandex.ru>
2018-12-17 12:07:41 +0300

    Update AlarmEventProcessor.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmEventProcessor.java
@@ -4,17 +4,16 @@ public class AlarmEventProcessor implements EventProcessor {
     @Override
     public void processEvent(SmartHome smartHome, SensorEvent sensorEvent) {
         if(this.isAlarmEvent(sensorEvent)) {
+
             AlarmSensorEvent event = (AlarmSensorEvent) sensorEvent;
+
             if (sensorEvent.getType() == SensorEventType.ALARM_ACTIVATE) {
-                smartHome.getAlarmSystem().changeState(new ActivateAlarmState(smartHome.getAlarmSystem()));
                 smartHome.getAlarmSystem().activate(event.getCode());
             }
             else {
-                smartHome.getAlarmSystem().changeState(new DeactivateAlarmState(smartHome.getAlarmSystem()));
                 smartHome.getAlarmSystem().deactivate(event.getCode());
             }
             if (event.getCode().hashCode() != smartHome.getAlarmSystem().getCode()) {
-                smartHome.getAlarmSystem().changeState(new SetAlarmState(smartHome.getAlarmSystem()));
                 smartHome.getAlarmSystem().setAlarm();
             }
         }
