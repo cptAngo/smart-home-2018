8a39ea67960d4591fc9d525c2d336dd4f80744bf
cptAngo <leha.andreevich@yandex.ru>
2018-12-17 12:08:34 +0300

    Update ActivateAlarmState.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/ActivateAlarmState.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/ActivateAlarmState.java
@@ -8,20 +8,28 @@ public class ActivateAlarmState implements AlarmState {
     public ActivateAlarmState(AlarmSystem alarmSystem) {
         this.alarmSystem = alarmSystem;
     }
+
     @Override
     public void activate(String pass) {
-        if (pass.hashCode() == this.alarmSystem.getCode()) {
-            this.alarmSystem.changeState(this);
+        if (pass.hashCode() != this.alarmSystem.getCode()) {
+            this.setAlarm();
         }
     }
 
     @Override
     public void deactivate(String  pass) {
+        if (pass.hashCode() == this.alarmSystem.getCode()) {
+            this.alarmSystem.changeState(new DeactivateAlarmState(this.alarmSystem));
+        }
+        else {
+            this.setAlarm();
+        }
 
     }
 
     @Override
     public void setAlarm() {
+        this.alarmSystem.changeState(new SetAlarmState( this.alarmSystem));
 
     }
 }
