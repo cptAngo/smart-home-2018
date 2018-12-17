957f87f7f635aeae1b8081a611786523acee125d
cptAngo <leha.andreevich@yandex.ru>
2018-12-17 12:07:18 +0300

    Update AlarmSystemTest.java

--- smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmSystemTest.java
+++ smart-home-2018-master/src/main/java/ru/sbt/mipt/oop/AlarmSystemTest.java
@@ -10,17 +10,17 @@ public class AlarmSystemTest {
     @Test
     public void testNewSystemOff() {
         AlarmSystem alarmSystem = new AlarmSystem();
-        assertEquals(AlarmSystemState.OFF, alarmSystem.getState());
+        assertEquals(AlarmSystemState.OFF, alarmSystem.getType());
     }
 
     @Test
     public void testTurnSystemOn() {
         AlarmSystem alarmSystem = new AlarmSystem();
         alarmSystem.activate("wrong code");
-        assertEquals(AlarmSystemState.ON, alarmSystem.getState());
+        assertEquals(AlarmSystemState.ON, alarmSystem.getType());
 
         alarmSystem.activate("default_password");
-        assertEquals(AlarmSystemState.ON, alarmSystem.getState());
+        assertEquals(true, alarmSystem.isActivated());
     }
 
     @Test
